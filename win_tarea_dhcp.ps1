d989d85e9042de08994028de273cde22b53401ba
Gael-sl <gaelarsenio123@gmail.com>
2025-02-24 14:26:48 -0700

    Create win_tarea_dhcp.ps1

---
+++ win_tarea_dhcp.ps1
@@ -0,0 +1,43 @@
+﻿#instalar el dhcp
+
+Write-Host "instalando dhcp"
+
+Install-WindowsFeature -Name DHCP IncludeManagement Tools
+
+#info
+
+$subnet Read-Host "ingresa la subred"
+
+$subnetMask Read-Host "ingresa la Mask de subred" $startIP Read-Host "ingresa la ip del inicio del rango"
+
+SendIP Read-Host "ingresa la ip final del rango"
+
+$gateway Read-Host "ingresa el gateway"
+
+$dnsServer Read-Host "Ingresa la ip del servidor dns"
+
+#crea un scope
+
+Write-Host "creando scope"
+
+Add-DhcpServerv4Scope -Name "RedInterna" -StartRange $startIP-EndRange SendIP -SubnetMask $subnetMask -State Active
+
+#configura el gateway
+
+Write-Host "configurando gateway"
+
+Set-DhcpServerv40ptionValue -ScopeId $subnet-OptionId 3 -Value $gateway
+
+#configura dns
+
+Write-Host "configurando dns"
+
+Set-DhcpServerv40ptionValue -Scopeld $subnet -OptionId 6 -Value $dnsServer
+
+#iniciar el dhcp
+
+Write-Host "iniciando dhcp"
+
+Restart-Service dhcpserver
+
+Write-Host "configuracion de todo al 100"
